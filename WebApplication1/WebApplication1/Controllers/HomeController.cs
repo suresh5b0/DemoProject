abf8f5ede2310b43baeb733b13922455780b19fb
Suresh <s.suresh5b0@gmail.com>
2017-03-26 10:19:47 +0530

    updated the controller

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.dddddddfdfdf";
+            ViewBag.Message = "Your application description page.dddddddfdfdf tftttttytytyttytyftft";
 
             return View();
         }
